60fd8ef2b9ee142c32470e8ca14efa4bf6d4ec52
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-11 11:04:03 +0200

    complete Basic Ex 1

---
+++ Dmitrii/Basic/script.js
@@ -0,0 +1,15 @@
+"use strict";
+// ////////////////// //
+// /// Basic Ex 1 /// //
+// ////////////////// //
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(numbers);
+for (let i = 1; i <= numbers.length; i++) {
+    console.log(`Nummber ${i}`);
+    for (let j = 1; j <= numbers.length; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+}
+// ////////////////// //
+// /// Basic Ex 2 /// //
+// ////////////////// //
