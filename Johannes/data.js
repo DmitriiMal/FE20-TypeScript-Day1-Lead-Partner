7948db06f322708d5d3f5da32a729adc0a9e3a6e
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-11 14:49:38 +0200

    add changes

--- Johannes/data.js
+++ Johannes/data.js
@@ -3,19 +3,19 @@ let products = [
     {
         name: "the green one",
         image: "https://cdn.pixabay.com/photo/2014/12/16/23/45/soup-570922_1280.jpg",
-        price: 3.5,
+        price: 3.75,
         qtty: 1
     },
     {
         name: "the orange one",
         image: "https://cdn.pixabay.com/photo/2016/09/21/20/22/pumpkin-soup-1685586_1280.jpg",
-        price: 4.2,
+        price: 4.75,
         qtty: 1
     },
     {
         name: "the one with noodles",
         image: "https://cdn.pixabay.com/photo/2016/04/17/17/55/soup-1335168_1280.jpg",
-        price: 5.5,
+        price: 5.75,
         qtty: 1
     },
     {
@@ -25,27 +25,27 @@ let products = [
         qtty: 1
     },
     {
-        name: "the green one",
+        name: "the other green one",
         image: "https://cdn.pixabay.com/photo/2014/12/16/23/45/soup-570922_1280.jpg",
-        price: 3.5,
+        price: 3.25,
         qtty: 1
     },
     {
-        name: "the orange one",
+        name: "the other orange one",
         image: "https://cdn.pixabay.com/photo/2016/09/21/20/22/pumpkin-soup-1685586_1280.jpg",
-        price: 4.2,
+        price: 4.25,
         qtty: 1
     },
     {
-        name: "the one with noodles",
+        name: "the otherone with noodles",
         image: "https://cdn.pixabay.com/photo/2016/04/17/17/55/soup-1335168_1280.jpg",
-        price: 5.5,
+        price: 5.25,
         qtty: 1
     },
     {
-        name: "the red one",
+        name: "the other red one",
         image: "https://cdn.pixabay.com/photo/2015/02/03/16/31/soup-622737_1280.jpg",
-        price: 4.75,
+        price: 4.25,
         qtty: 1
     }
 ];
