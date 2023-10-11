89bb1b68c041b9e6e0d7af72e8a543cb104be829
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-11 11:25:06 +0200

    add b1

---
+++ Johannes/script.ts
@@ -0,0 +1,17 @@
+// let firstNr: number = 1;
+// let secondNr: number = 1;
+// let product: number = firstNr * secondNr;
+let product: number;
+
+let nrArray:number[] = [1,2,3,4,5,6,7,8,9,10];
+for(let i = 0; i < nrArray.length; i++){
+    let arrayResult = document.getElementById(`resultB1`) as HTMLElement;
+    for(let j = 0; j < nrArray.length; j++){
+        product = nrArray[i] * nrArray[j];
+        // console.log(`${nrArray[i]} * ${nrArray[j]} = ${product}`);
+        arrayResult.innerHTML += `
+        <p>${nrArray[i]} * ${nrArray[j]} = ${product}<p>
+        `;
+    }
+    arrayResult.innerHTML += `<hr>`;
+}
