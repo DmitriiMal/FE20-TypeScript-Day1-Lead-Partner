1a73705dfa05e6bdfc6a396fd0122b538662fe29
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-11 13:45:56 +0200

    add changes

---
+++ Johannes/data.js
@@ -0,0 +1,27 @@
+"use strict";
+let products = [
+    {
+        name: "the green one",
+        image: "https://cdn.pixabay.com/photo/2014/12/16/23/45/soup-570922_1280.jpg",
+        price: 3.5,
+        qtty: 1
+    },
+    {
+        name: "the orange one",
+        image: "https://cdn.pixabay.com/photo/2016/09/21/20/22/pumpkin-soup-1685586_1280.jpg",
+        price: 4.2,
+        qtty: 1
+    },
+    {
+        name: "the dark red one",
+        image: "https://cdn.pixabay.com/photo/2016/04/17/17/55/soup-1335168_1280.jpg",
+        price: 5.5,
+        qtty: 1
+    },
+    {
+        name: "the red one",
+        image: "https://cdn.pixabay.com/photo/2015/02/03/16/31/soup-622737_1280.jpg",
+        price: 4.75,
+        qtty: 1
+    }
+];
--- Johannes/script4.js
+++ Johannes/script4.js
@@ -1,1 +1,21 @@
 "use strict";
+//current object formatter
+const currencyFormater = new Intl.NumberFormat("de-AT", {
+    style: "currency",
+    currency: "EUR",
+});
+//select the products row and add items dynamically
+let productsRow = document.querySelector(".products");
+for (let product of products) {
+    productsRow.innerHTML += `
+        <div class="card product col my-4" style="width: 300px;">
+            <img class="card-img-top mt-2 px-3" src="${product.image}" alt="${product.name}">
+            <div class="card-body px-3 py-0">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, libero.</p>
+                <p class="card-text h3 text-end">${currencyFormater.format(product.price)}</p>
+                <p class="card-text3 d-flex justify-content-end"><button class="btn w-75 product-button"><i class="fs-4 bi bi-cart-plus"></i> Add to cart</button></p>
+            </div>
+        </div>
+    `;
+}
