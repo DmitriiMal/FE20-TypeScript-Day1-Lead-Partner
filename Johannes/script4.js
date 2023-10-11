be8b5fa17db538aaa79898f8cc57800fa06996d2
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-11 14:11:18 +0200

    add changes

--- Johannes/script4.js
+++ Johannes/script4.js
@@ -14,8 +14,58 @@ for (let product of products) {
                 <h5 class="card-title">${product.name}</h5>
                 <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, libero.</p>
                 <p class="card-text h3 text-end">${currencyFormater.format(product.price)}</p>
-                <p class="card-text3 d-flex justify-content-end"><button class="btn w-75 product-button"><i class="fs-4 bi bi-cart-plus"></i> Add to cart</button></p>
+                <p class="card-text3 d-flex justify-content-end"><button class="btn w-75 product-button btn-outline-dark"><i class="fs-4 bi bi-cart-plus"></i> Add to cart</button></p>
             </div>
         </div>
     `;
 }
+//product button selected
+const addToCartBtn = document.querySelectorAll(".product-button");
+//add event to add to cart buttons
+addToCartBtn.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        addToCart(products[i]);
+    });
+});
+//cart declared
+const cart = [];
+const addToCart = (product) => {
+    if (cart.find((val) => val.name == product.name)) {
+        // console.log(cart.find((val) => val.name == product.name));
+        product.qtty++;
+    }
+    else {
+        cart.push(product);
+    }
+    console.table(cart);
+    createRows();
+};
+const createRows = () => {
+    let result = "";
+    for (let item of cart) {
+        result += `
+            <div class="cart-row row gx-0">
+                <div class="cart-item col-6 col-sm-6 my-2 d-flex align-items-center justify-content-center">
+                        <img class="cart-item-image" src="${item.image}" height="100" alt="${item.name}">
+                        <div class="cart-item-title h5 ms-2">${item.name}</div>
+                    </div>
+                    <div class="cart-qtty-action col-2 d-flex justify-content-center align-items-center">
+                        <div class="d-flex">
+                            <i class="minus fs-5 bi bi-dash-circle-fill"></i>
+                        </div>
+                        <div class="text-center m-0 cart-quantity h4 w-25">${item.qtty}</div>
+                        <div class="d-flex">
+                            <i class="plus fs-5 bi bi-plus-circle-fill"></i>
+                        </div>
+                    </div>
+                    <div class="col-1 d-flex justify-content-start align-items-center">
+                        <i class="del fs-4 bi bi-trash3-fill text-danger"></i>
+                    </div>
+                <div class="cart-price col-3 h5 my-auto text-end pe-5">${currencyFormater.format(item.price)}</div>
+            </div>                    
+        </div>
+        `;
+    }
+    let cartItems = document.querySelector(".cart-items");
+    cartItems.innerHTML = result;
+};
