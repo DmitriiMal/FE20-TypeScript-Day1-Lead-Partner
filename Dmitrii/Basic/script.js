e73a9c055e0c5af5b745cfb65c36af8db754cbb4
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-11 11:56:06 +0200

    complete Basic Ex 2

--- Dmitrii/Basic/script.js
+++ Dmitrii/Basic/script.js
@@ -2,14 +2,75 @@
 // ////////////////// //
 // /// Basic Ex 1 /// //
 // ////////////////// //
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// console.log(numbers);
-for (let i = 1; i <= numbers.length; i++) {
-    console.log(`Nummber ${i}`);
-    for (let j = 1; j <= numbers.length; j++) {
-        console.log(`${i} * ${j} = ${i * j}`);
-    }
-}
+// let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 1; i <= numbers.length; i++) {
+//   // console.log(`Nummber ${i}`);
+//   let ex1 = document.getElementById('ex1') as HTMLElement;
+//   ex1.innerHTML += `<hr>Nummber ${i}`;
+//   for (let j = 1; j <= numbers.length; j++) {
+//     // console.log(`${i} * ${j} = ${i * j}`);
+//     ex1.innerHTML += `<p>${i} * ${j} = ${i * j}<p>`;
+//   }
+// }
 // ////////////////// //
 // /// Basic Ex 2 /// //
+// ////////////////// //
+let fullName = [
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+];
+let ex2 = document.getElementById('ex2');
+ex2.innerHTML += `${fullName[0].fName} ${fullName[0].lName} <br>`;
+let fullNameArr = [
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+    {
+        fName: 'Dmitrii',
+        lName: 'Malyshkin',
+    },
+];
+let ex22 = document.getElementById('ex2-2');
+setTimeout(() => {
+    fullNameArr.forEach((element) => {
+        ex22.innerHTML += `<p>${element.fName}</p>`;
+    });
+}, 5000);
+// ////////////////// //
+// /// Basic Ex 3 /// //
 // ////////////////// //
